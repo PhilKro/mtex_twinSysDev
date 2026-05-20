fc502db856359aff2b3da41f2abc2e526eb13f77
PhilKro <56341570+PhilKro@users.noreply.github.com>
2026-05-20 17:07:17 +0200

    Miller round now can force rounding in 3 index notation returns the rounding error and Twin Type determintation seems to work correctly, based on rationality of indices

--- geometry/@Miller/round.m
+++ geometry/@Miller/round.m
@@ -1,36 +1,45 @@
-function h = round(h,varargin)
+function [h, err] = round(h,varargin)
 % tries to round miller indizes to greatest common divisor
 %
 % Syntax
 %   h = round(h)
 %   h = round(h,'maxHKL',5)
-%   h = round(h,'use4Index')
+%   h = round(h,'round3Index')
+%   [h, err] = round(...)
 %
 % Input
 %  h - @Miller
 %
 % Output
 %  h - @Miller
+%  err - angle between original and rounded Miller vector
 %
 % Options
 %  maxHKL - maximum value of Miller indices
-%  use4Index - flag to decide if all 4 indices should be used for rounding
+%  round3Index - flag to force rounding the 3 index notation for Tri/Hex lattices
 %
 
 % ignore xyz case
-if h.dispStyle == MillerConvention.xyz, return; end
+if h.dispStyle == MillerConvention.xyz, err = 0; return; end
 
+h_old = h;
 sh = size(h);
 
-mOld = h.coordinates;
+% check if we should force 3 index rounding for Tri/Hex
+round3Index = check_option(varargin, 'round3Index') && h.lattice.isTriHex;
 
-% check if 4 index notation should be used
-use4Index = check_option(varargin, 'use4Index');
+isDirection = any(strcmpi(char(h(1).dispStyle), {'uvw','UVTW'}));
 
-% consider only 3 digits Miller indices unless use4Index is specified
-if use4Index && size(mOld, 2) == 4
+if round3Index
+  if isDirection
+      mOld = h.uvw;
+  else
+      mOld = h.hkl;
+  end
   mOld = mOld';
 else
+  mOld = h.coordinates;
+  % consider only 3 digits Miller indices
   mOld = mOld(:,[1 2 end])';
 end
 
@@ -55,13 +64,23 @@ end
 
 h = h .* multiplier;
 
-% remember current dispStyle
 currentDispStyle = h.dispStyle;
 
 % now round
-h.coordinates = round(h.coordinates);
+if round3Index
+  if isDirection
+      h.uvw = round(h.uvw);
+  else
+      h.hkl = round(h.hkl);
+  end
+else
+  h.coordinates = round(h.coordinates);
+end
 
-% restore dispStyle to prevent breaking it (e.g. hkl changing to hkil)
 h.dispStyle = currentDispStyle;
 
 h = reshape(h,sh);
+
+if nargout == 2
+    err = angle(h_old, h);
+end
